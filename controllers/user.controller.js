44547237983ce38d5ce6a684aad6109489b7f0db
Sujal <palsujal707@gmail.com>
2024-11-19 23:55:15 +0530

    adding user functionalities

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,10 +1,21 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler( async (req, res) => {
-    res.status(200).json({
-        message: "ok"
-    })
-} )
+    //get user details
+    //validation
+    //check user if already exist - email, username
+    //check for images ,  avatar
+    //upload them to cloudinary, avatar
+    //create user objects
+    //remove passsword and refresh token
+    //check for user creation
+    //return res
+
+    
 
 
-export {registerUser}
+
+})
+
+
+export {registerUser} 
